979783910d35b643d8c98aed545d815f465877ca
gitzaan <fowzaan.rasheed@gmail.com>
2025-03-26 10:47:28 +0530

    Docker

--- src/app/keycloak.config.ts
+++ src/app/keycloak.config.ts
@@ -1,7 +1,7 @@
 import { KeycloakConfig } from 'keycloak-js';
 
 const keycloakConfig: KeycloakConfig = {
-  url: 'http://localhost:8080',
+  url: 'http://localhost:8080',  
   realm: 'siemens-login',
   clientId: 'my-app'
 };
